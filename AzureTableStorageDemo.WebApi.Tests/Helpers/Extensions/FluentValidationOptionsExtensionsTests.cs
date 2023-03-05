823ea76ac8d78fa32fd4d5c8568d1a434bd6f87d
RAKESH KUMAR <rakeshbimra@outlook.com>
2023-03-05 14:29:12 +0530

    added unit test cases for extension methods

---
+++ AzureTableStorageDemo.WebApi.Tests/Helpers/Extensions/FluentValidationOptionsExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using AzureTableStorageDemo.WebApi.Helpers.Extensions;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTableStorageDemo.WebApi.Tests.Helpers.Extensions
+{
+    [TestClass]
+    public class FluentValidationOptionsExtensionsTests
+    {
+        [TestMethod]
+        public void AddWithValidation_ShouldAddValidatorAndOptions_WhenCalled()
+        {
+            // Arrange
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(config);
+
+            // Act
+            services.AddWithValidation<TestOptions, TestOptionsValidator>("TestOptions");
+
+            // Assert
+            var provider = services.BuildServiceProvider();
+            var options = provider.GetService<IOptions<TestOptions>>();
+            var validator = provider.GetService<IValidator<TestOptions>>();
+
+            Assert.IsNotNull(options);
+            Assert.IsNotNull(validator);
+        }
+    }
+
+    public class TestOptions
+    {
+        public string Name { get; set; }
+    }
+
+    public class TestOptionsValidator : AbstractValidator<TestOptions>
+    {
+        public TestOptionsValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
---
+++ AzureTableStorageDemo.WebApi.Tests/Helpers/Extensions/OptionsBuilderFluentValidationExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using AzureTableStorageDemo.WebApi.Helpers.Configurations.Validators;
+using AzureTableStorageDemo.WebApi.Helpers.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTableStorageDemo.WebApi.Tests.Helpers.Extensions
+{
+    [TestClass]
+    public class OptionsBuilderFluentValidationExtensionsTests
+    {
+        [TestMethod]
+        public void ValidateFluentValidation_ShouldRegisterValidator_WhenCalled()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var optionsBuilder = new OptionsBuilder<TestOptions>(services, "myOptions");
+
+            // Act
+            optionsBuilder.ValidateFluentValidation();
+
+            // Assert
+            var serviceProvider = services.BuildServiceProvider();
+            var validateOptions = serviceProvider.GetService<IValidateOptions<TestOptions>>();
+            Assert.IsNotNull(validateOptions);
+            Assert.IsInstanceOfType(validateOptions, typeof(FluentValidationOptions<TestOptions>));
+        }
+    }
+}
